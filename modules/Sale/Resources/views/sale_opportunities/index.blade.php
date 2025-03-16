0eace661e81b75b3c5300ea46c5c8a502c6db1f9
Josue-bp <josueborja1231@gmail.com>
2025-03-15 21:26:23 -0500

    fix(sale): actualización en index.blade.php

--- modules/Sale/Resources/views/sale_opportunities/index.blade.php
+++ modules/Sale/Resources/views/sale_opportunities/index.blade.php
@@ -1,6 +1,13 @@
 @extends('tenant.layouts.app')
 
 @section('content')
+    @if (session('error'))
+        <script>
+            window.onload = function() {
+                alert("{{ session('error') }}");
+            };
+        </script>
+    @endif
     <tenant-sale-opportunities-index
         :type-user="{{json_encode(Auth::user()->type)}}"
         :can-generate  = "{!! $SellerCanGenerateSaleOpportunities==false?'false':'true' !!}"
